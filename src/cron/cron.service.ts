77a931d08c2445a497360d5529cf75bf150a93d3
Philip Damra <pdamra@PDamraMBP.local>
2023-11-24 14:07:33 -0700

    Use metadataScanner to get all method names, loop over them and use the reflector to see if they have the method decorator, and if so, use the method in a setInterval

--- src/cron/cron.service.ts
+++ src/cron/cron.service.ts
@@ -1,4 +1,10 @@
 import { IntervalHost } from 'src/scheduler/decorators/interval-host.decorator';
+import { Interval } from 'src/scheduler/decorators/interval.decorator';
 
 @IntervalHost
-export class CronService {}
+export class CronService {
+  @Interval(1000)
+  everySecond() {
+    console.log('Every second this will run');
+  }
+}
--- src/scheduler/interval.scheduler.ts
+++ src/scheduler/interval.scheduler.ts
@@ -1,12 +1,21 @@
-import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
-import { DiscoveryService, Reflector } from '@nestjs/core';
+import {
+  Injectable,
+  OnApplicationBootstrap,
+  OnApplicationShutdown,
+} from '@nestjs/common';
+import { DiscoveryService, MetadataScanner, Reflector } from '@nestjs/core';
 import { INTERVAL_HOST_KEY } from './decorators/interval-host.decorator';
+import { INTERVAL_KEY } from './decorators/interval.decorator';
 
 @Injectable()
-export class IntervalScheduler implements OnApplicationBootstrap {
+export class IntervalScheduler
+  implements OnApplicationBootstrap, OnApplicationShutdown
+{
+  private readonly intervalRefs: NodeJS.Timeout[] = [];
   constructor(
     private readonly discoveryService: DiscoveryService,
     private readonly reflector: Reflector,
+    private readonly metadataScanner: MetadataScanner,
   ) {}
 
   onApplicationBootstrap() {
@@ -24,6 +33,27 @@ export class IntervalScheduler implements OnApplicationBootstrap {
       if (isIntervalHost) {
         console.log('IntervalHost found', wrapper.token);
       }
+
+      const methodKeys = this.metadataScanner.getAllMethodNames(prototype);
+      console.log('methodKeys', methodKeys);
+      // Get all the method names of the classes marked with the class decorator
+      methodKeys.forEach((methodKey) => {
+        // Find which methods have the method decorator
+        const interval = this.reflector.get(INTERVAL_KEY, instance[methodKey]);
+        if (!interval) {
+          return;
+        }
+        console.log('Interval found', wrapper.token, methodKey);
+        // For the methods that have the method decorator, call on the interval
+        const intervalRef = setInterval(() => instance[methodKey](), interval);
+        this.intervalRefs.push(intervalRef);
+      });
     });
+  }
+
+  onApplicationShutdown(signal?: string) {
+    // Clean up on application shutdown
+    console.log('cleaning up intervals at shutdown');
+    this.intervalRefs.forEach((intervalId) => clearInterval(intervalId));
   }
 }
