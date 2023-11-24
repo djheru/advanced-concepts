4bdd6c57ac42fae9aabd3c6ee5a3af0ccfffac74
Philip Damra <pdamra@PDamraMBP.local>
2023-11-24 13:52:22 -0700

    Use reflector to get classes that have been decorated with a specific decorator using metadata

---
+++ src/cron/cron.service.ts
@@ -0,0 +1,4 @@
+import { IntervalHost } from 'src/scheduler/decorators/interval-host.decorator';
+
+@IntervalHost
+export class CronService {}
--- src/scheduler/interval.scheduler.ts
+++ src/scheduler/interval.scheduler.ts
@@ -1,14 +1,29 @@
 import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
-import { DiscoveryService } from '@nestjs/core';
+import { DiscoveryService, Reflector } from '@nestjs/core';
+import { INTERVAL_HOST_KEY } from './decorators/interval-host.decorator';
 
 @Injectable()
 export class IntervalScheduler implements OnApplicationBootstrap {
-  constructor(private readonly discoveryService: DiscoveryService) {}
+  constructor(
+    private readonly discoveryService: DiscoveryService,
+    private readonly reflector: Reflector,
+  ) {}
 
   onApplicationBootstrap() {
     const providers = this.discoveryService.getProviders();
     providers.forEach((wrapper) => {
-      console.log(wrapper.token);
+      const { instance } = wrapper;
+      const prototype = instance && Object.getPrototypeOf(instance);
+      if (!instance || !prototype) {
+        // Will exclude value and factory providers, only interested in intastiated classes
+        return;
+      }
+      const isIntervalHost = // Class with interval decorator
+        this.reflector.get(INTERVAL_HOST_KEY, instance.constructor) ?? false;
+
+      if (isIntervalHost) {
+        console.log('IntervalHost found', wrapper.token);
+      }
     });
   }
 }
