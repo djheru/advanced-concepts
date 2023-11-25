01412daaca062582b9dd81cb47c46014beae5552
Philip Damra <pdamra@PDamraMBP.local>
2023-11-24 22:24:44 -0700

    Using Piscina to abstract away some implementation details and boilerplate from workers

--- src/fibonacci/fibonacci.controller.ts
+++ src/fibonacci/fibonacci.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get, Query } from '@nestjs/common';
-import { FibonacciWorkerHost } from './fibonacci-worker.host';
+import { resolve } from 'path';
+import Piscina from 'piscina';
 
 @Controller('fibonacci')
 export class FibonacciController {
-  constructor(private readonly fibonacciWorkerHost: FibonacciWorkerHost) {}
-
+  fibonacciWorker = new Piscina({
+    filename: resolve(__dirname, './fibonacci.worker.js'),
+  });
   @Get()
   fibonacci(@Query('n') n = 10) {
-    return this.fibonacciWorkerHost.run(n);
+    return this.fibonacciWorker.run(n);
   }
 }
--- src/fibonacci/fibonacci.worker.ts
+++ src/fibonacci/fibonacci.worker.ts
@@ -1,18 +1,8 @@
-import { parentPort } from 'worker_threads';
-
 function fib(n: number) {
   if (n < 2) {
     return n;
   }
   return fib(n - 1) + fib(n - 2);
 }
-
-parentPort.on('message', ({ n: numberToBeCalculated, id: uniqueRequestId }) => {
-  const result = fib(numberToBeCalculated);
-  parentPort.postMessage({
-    result,
-    id: uniqueRequestId,
-  });
-});
 
 module.exports = (n: number) => fib(n);
