cb47471e4432b13d9dfab2544f9381a259cd6233
Philip Damra <pdamra@PDamraMBP.local>
2023-11-25 19:29:11 -0700

    Add configurable model functionality to the schematic, to include a module-definition file, with a name auto-generated from the passed in options using dasherize

--- schematics/src/configurable-module/index.ts
+++ schematics/src/configurable-module/index.ts
@@ -2,18 +2,33 @@ import {
   Rule,
   SchematicContext,
   Tree,
+  apply,
   chain,
   externalSchematic,
+  mergeWith,
+  move,
+  strings,
+  template,
+  url,
 } from '@angular-devkit/schematics';
+
+interface ConfigurableModuleSchematicOptions {
+  name: string;
+}
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function generate(_options: any): Rule {
+export function generate(options: ConfigurableModuleSchematicOptions): Rule {
   return (_tree: Tree, _context: SchematicContext) => {
+    const templateSource = apply(url('./files'), [
+      template({ ...options, ...strings }),
+      move('src'),
+    ]);
     return chain([
       externalSchematic('@nestjs/schematics', 'module', {
-        name: 'placeholder',
+        name: options.name,
       }),
+      mergeWith(templateSource),
     ]);
   };
 }
